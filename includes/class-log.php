e7f16ddeaa7bf4adcbac5d0169c7bb367efd89dd
Gary Kovar <gkovar@gmail.com>
2017-03-08 21:58:04 -0500

    Echoes some data.

--- includes/class-log.php
+++ includes/class-log.php
@@ -39,6 +39,26 @@ class CS_Log {
 	 * @since  0.1.0
 	 */
 	public function hooks() {
+		add_action( 'admin_menu', array( $this, 'add_submenu' ) );
+	}
 
+	public function add_submenu() {
+		add_submenu_page( 'tools.php', 'Cron Run History', 'Cron Run History', 'manage_options', 'cron_status_log', array( $this, 'display_cron_history' ) );
+	}
+
+	public function display_cron_history() {
+		echo '<H1>Cron History</H1>';
+
+		$crons = get_option( 'cron_stats' );
+		if ( is_array( $crons ) ) {
+			echo count( $crons ) . ' logged cron executions in the last 24 hours<BR />';
+
+			$keys = array_keys($crons);
+
+			$difference = end( $keys ) - reset( $keys );
+			$average = $difference/count($crons);
+
+			echo 'You\'re averaging WP_Cron execution every ' . (int)$average . ' seconds<BR />';
+		}
 	}
 }
